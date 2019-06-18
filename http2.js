e94fbb7ae9205ee0e55f7b41627633168df4e967
derbysofti72 <rzh881004@icloud.com>
2019-06-18 16:13:27 +0800

    commit: test

--- http2.js
+++ http2.js
@@ -2,7 +2,7 @@ const http2 = require('http2');
 const path = require('path')
 const express = require('express')
 const fs = require('fs');
-const { HTTP2_HEADER_PATH } = http2.constants
+const { HTTP2_HEADER_PATH, NGHTTP2_REFUSED_STREAM } = http2.constants
 
 const server = http2.createSecureServer({
     key: fs.readFileSync(__dirname + '/configs/server.key'),
@@ -10,38 +10,56 @@ const server = http2.createSecureServer({
 });
 
 // Push file
-function push(stream, path) {
+function push(stream, path, contentType) {
     console.log(__dirname + path)
+    try {
+        stream.pushStream({ [HTTP2_HEADER_PATH]: path }, (err, pushStream, headers) => {
+            // console.log(err)
+            // console.log(pushStream)
+            // console.log(headers)
+            if (err) throw err;
+            try {
+                pushStream.respond({
+                    'content-type': contentType,
+                    ':status': 200
+                })
+                pushStream.end(fs.readFileSync(__dirname + '/web' + path))
+            } catch (error) {
+                throw error
+            }
 
-    stream.pushStream({ [HTTP2_HEADER_PATH]: path }, (err, pushStream, headers) => {
-        // console.log(err)
-        // console.log(pushStream)
-        // console.log(headers)
-        if (err) throw err;
-        pushStream.respond({
-            'content-type': 'application/javascript',
-            ':status': 200
         })
-        pushStream.end(fs.readFileSync(__dirname + path))
-    })
+    } catch (error) {
+        throw error
+    }
 }
 
 server.on('error', (err) => console.error(err));
 
 server.on('stream', (stream, headers) => {
-    if (headers[':path'] === '/home') {
-        const fd = fs.openSync(__dirname + '/web/index.html', 'r');
+    // if (headers[':path'] === '/home') {
+    // if (headers[':method'] !== 'CONNECT') {
+    //     // Only accept CONNECT requests
+    //     stream.close(NGHTTP2_REFUSED_STREAM);
+    //     return;
+    // }
+    const fd = fs.openSync(__dirname + '/web/index.html', 'r');
+    // stream 是一个双工流
+    stream.respond({
+        'content-type': 'text/html',
+        ':status': 200
+    });
+    // if (stream.pushAllowed) {
+    push(stream, '/bundle1.js', 'application/javascript')
+    push(stream, '/bundle2.js', 'application/javascript')
+    push(stream, '/jquery-migrate-1.4.1.js', 'application/javascript')
+    push(stream, '/style.css', 'text/css')
 
-        // stream 是一个双工流
-        stream.respond({
-            'content-type': 'text/html',
-            ':status': 200
-        });
-        push(stream, '/web/bundle1.js')
-        push(stream, '/web/bundle2.js')
-        stream.end(fs.readFileSync(__dirname + '/web/index.html'));
-    }
+
+    stream.end(fs.readFileSync(__dirname + '/web/index.html'));
+    // }
+    // }
 });
 
-server.listen(3030);
+server.listen(8888);
 
